efa409e2bac1f9f864963ec6dff702a1fdc76c9b
Fotis Rokanis <84749437+frokanic@users.noreply.github.com>
2024-06-17 21:53:08 +0300

    Some refactoring.

--- 004_control_structures/loops.py
+++ 004_control_structures/loops.py
@@ -7,13 +7,16 @@ There are 2 ways to "repeat something" in Python. For and while loops.
 def main():
     single_digit_odd_numbers()
 
+
 def hi_there():
     for _ in range(3):
         print("Hi there :)")
 
+
 def single_digit_integers_squared():
     for num in range(1, 6):
         print("Number", num, "squared is", num**2)
+
 
 def single_digit_odd_numbers():
     for num in range(1, 10, 2):
